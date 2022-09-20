f504e3b80bb5bdae6015a0ec83feb23f86dc5946
sean <midruss@yahoo.com>
2022-09-20 10:03:22 -0400

    refactor cell.jsx

--- src/cell.jsx
+++ src/cell.jsx
@@ -4,28 +4,34 @@ import {useState} from 'react';
 function Cell(props) {
 
 const [isAlive, setIsAlive] = useState(false)
-const [board, setBoard] = useState(props.board)
+// const [board, setBoard] = useState(props.board)
+// const [change, setChange] = useState([])
+const [count, setCount] = useState(0)
 
-function handleAlive() {
-    debugger
+
+function handleAlive(pos) {
     setIsAlive(!isAlive)
-    debugger
-    console.log(board)
-    const newBoard = board.map((arr) => arr.slice());
-    debugger
-    newBoard[props.r][props.c] = 1
-    debugger
-    setBoard(newBoard)
-    debugger
+    setCount(prevCount => prevCount += 1)
+    // const newChange = []
+    // newChange.push([pos])
+    // setChange(prevChange => setChange(prevChange.push([pos])))
+    // const newBoard = board.map((arr) => arr.slice());
+    // newBoard[props.r][props.c] = 1
+    // setBoard(newBoard)
+    // setBoard(prevBoard => setBoard(prevBoard[props.r][props.c] = 1))
+    // setBoard((prevBoard) => setBoard(prevBoard[props.r][props.c] = 1))
 }
 
 console.log(isAlive)
-console.log(board)
+// console.log(count)
+// console.log(change)
+// console.log(props.pos)
+// console.log(board)
 
 return (
     <div>
         {!isAlive ?
-            <p style={{background:"red", height:"10px", width:"10px", cursor:"pointer"}} onClick={handleAlive}>{props.pos}</p> :
+            <p style={{background:"red", height:"10px", width:"10px", cursor:"pointer"}} onClick={() => handleAlive([props.r, props.c])}>{props.pos}</p> :
             <p style={{background:"black", height:"10px", width:"10px", cursor:"pointer"}} onClick={handleAlive}>{props.pos}</p>
         }
     </div>
