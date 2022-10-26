22c1daa0804b586fac625696d059e0359e038e89
sean <midruss@yahoo.com>
2022-10-26 11:00:39 -0400

    update meta tag gif

--- src/game.jsx
+++ src/game.jsx
@@ -63,7 +63,7 @@ function Game() {
                 setGeneration(prevGeneration => prevGeneration + 1)
                 setNewObj(nextGeneration)
                 console.log(newObj)
-            }, 10);
+            }, 70);
             return (()=> clearInterval(newIntervalId))
         }
     })
